a0ccf261e1540cff27a78ebdd718730a5caf5bd0
Alberto González Balderas <albertoglezb@hotmail.com>
2019-10-07 16:33:54 -0500

    Se terminó la clase: ActionResults.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,11 +10,16 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies
+        // public ViewResult
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Glory road." };
 
-            return View(movie);
+            //return View(movie);
+            //return Content("Hola Mundo");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
         }
     }
 }
