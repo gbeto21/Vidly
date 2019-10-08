0e8ab87e9a600a7b527bef8fa4fe1d4461933b59
Alberto González Balderas <albertoglezb@hotmail.com>
2019-10-08 08:35:45 -0500

    Se terminó la clase: Accion Parameters.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -15,11 +15,28 @@ namespace Vidly.Controllers
         {
             var movie = new Movie() { Name = "Glory road." };
 
-            //return View(movie);
+            return View(movie);
             //return Content("Hola Mundo");
             //return HttpNotFound();
             //return new EmptyResult();
-            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id = " + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (pageIndex.HasValue == false)
+                pageIndex = 1;
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex = {0} & sortBy = {1}",
+                pageIndex,
+                sortBy));
         }
     }
 }
